37375f0cdd4d6cbfc31be407bf6764dcff70f4f5
Aske Klitgaard Ottesen <Askeklitgaard@gmail.com>
2023-08-13 16:29:26 +0200

    change name

--- aarbs.sh
+++ aarbs.sh
@@ -7,7 +7,7 @@
 
 ### OPTIONS AND VARIABLES ###
 
-progsfile="https://raw.githubusercontent.com/askeko/AARBS/master/progs.csv"
+progsfile="https://raw.githubusercontent.com/askeko/AARBS-dwm/master/progs.csv"
 aurhelper="yay"
 repobranch="master"
 
